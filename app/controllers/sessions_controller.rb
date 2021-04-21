87a320026b4e11d004a1644089037de059080651
Eric <eric.m.lombardo@gmail.com>
2021-04-21 14:32:18 -0400

    Build login/logout with ActionController methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,18 @@
 class SessionsController < ApplicationController
+  
+  def new
+  end
 
-  def login
- 
+  def create
+    if !params[:name].present?  # if no name entered => redirects to login
+      redirect_to login_path 
+    else
+      session[:name] = params[:name]  # if name given => set session[:name] and redirect
+      redirect_to root_path 
+    end
+  end
+
+  def destroy
+    session.delete :name
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do  # looked at solution to confirm naming conventions
+  root "application#welcome"  
 
-  get "/login", to: "sessions#login"
-
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  get "/secret", to: "secrets#show"
+  post "/logout", to: "sessions#destroy"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
